86a41a58dedbae8ef283bd9fb28d173a2fb04d42
Punna Bhargavi <bhargavipunna5714@gmail.com>
2026-07-25 22:06:17 +0530

    Time: 0 ms (100%), Space: 8.7 MB (59.34%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,12 +1,30 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        string s = to_string(n);
-        sort(s.begin(), s.end(), greater<char>());
-        int first = s[0] - '0';
-        int second = s[1] - '0';
+        // string s = to_string(n);
+        // sort(s.begin(), s.end(), greater<char>());
+        // int first = s[0] - '0';
+        // int second = s[1] - '0';
         
-        return first * second;
+        // return first * second;
+
+        int max1 = 0;
+        int max2 = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            
+            if (digit > max1) {
+                max2 = max1;
+                max1 = digit;
+            } else if (digit > max2) {
+                max2 = digit;
+            }
+            
+            n /= 10;
+        }
+        
+        return max1 * max2;
+
 
     }
 };
