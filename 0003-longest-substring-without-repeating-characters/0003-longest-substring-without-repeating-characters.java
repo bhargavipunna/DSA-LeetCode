738e30e5ff54aa90e59eb471c1ed073a61c97a72
Punna Bhargavi <bhargavipunna5714@gmail.com>
2026-08-06 11:51:51 +0530

    Time: 4 ms (89.95%), Space: 47.8 MB (7.59%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -12,22 +12,36 @@ class Solution {
         //     max = Math.max(max,r-l+1);
         // }
         // return max;
-        HashMap<Character, Integer> map = new HashMap<>();
+        // HashMap<Character, Integer> map = new HashMap<>();
 
-        int left = 0;
-        int max = 0;
+        // int left = 0;
+        // int max = 0;
 
-        for (int right = 0; right < s.length(); right++) {
+        // for (int right = 0; right < s.length(); right++) {
+        //     char ch = s.charAt(right);
+
+        //     if (map.containsKey(ch)) {
+        //         left = Math.max(left, map.get(ch) + 1);
+        //     }
+
+        //     map.put(ch, right);
+        //     max = Math.max(max, right - left + 1);
+        // }
+
+        // return max;
+        int charIndex[] = new int[128];
+
+        int maxLength=0, left=0;
+        
+        for(int right = 0; right<s.length(); right++){
             char ch = s.charAt(right);
+            left = Math.max(left, charIndex[ch]);
 
-            if (map.containsKey(ch)) {
-                left = Math.max(left, map.get(ch) + 1);
-            }
+            charIndex[ch] = right + 1;
 
-            map.put(ch, right);
-            max = Math.max(max, right - left + 1);
+            maxLength = Math.max(maxLength,right - left + 1); 
         }
 
-        return max;
+        return maxLength;
     }
 }
