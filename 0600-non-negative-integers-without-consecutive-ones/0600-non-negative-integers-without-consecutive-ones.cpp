dd8bf0e7dd938128989144bac9303215cf779dda
Punna Bhargavi <bhargavipunna5714@gmail.com>
2026-08-11 20:00:44 +0530

    Time: 0 ms (100%), Space: 8.4 MB (47.77%) - LeetHub

--- 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     int findIntegers(int n) {
-        int dp[32][2][2];
-        memset(dp, -1, sizeof(dp));
-        
-        string s = "";
-        while (n > 0) {
-            s += (n % 2) ? '1' : '0';
-            n /= 2;
+        int dp[32];
+
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 2; i < 32; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        reverse(s.begin(), s.end());
-        
-        return solve(0, 0, 1, s, dp);
-    }
-    
-    int solve(int pos, int prev, int tight, const string& s, int dp[32][2][2]) {
-        if (pos == s.size()) return 1;
-        if (dp[pos][prev][tight] != -1) return dp[pos][prev][tight];
-        
-        int limit = tight ? (s[pos] - '0') : 1;
-        int count = 0;
-        
-        for (int digit = 0; digit <= limit; ++digit) {
-            if (prev == 1 && digit == 1) continue;
-            count += solve(pos + 1, digit, tight && (digit == limit), s, dp);
+
+        int ans = 0;
+        int prev = 0;
+
+        for (int i = 30; i >= 0; i--) {
+
+            if (n & (1 << i)) {
+
+                ans += dp[i];
+
+                if (prev == 1) {
+                    return ans;
+                }
+
+                prev = 1;
+            }
+            else {
+                prev = 0;
+            }
         }
-        
-        return dp[pos][prev][tight] = count;
+
+        return ans + 1;
     }
 };
