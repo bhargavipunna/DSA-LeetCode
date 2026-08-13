4b2db1cca23bc9b6228b1b816b0c945ab99ecd02
Punna Bhargavi <bhargavipunna5714@gmail.com>
2026-08-13 21:59:02 +0530

    Time: 3 ms (44.04%), Space: 8.5 MB (65.36%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+        return false;
+        long long rev=0;
+        int n=x;
+        while(x>0){
+            int d=x%10;
+            if (rev > (INT_MAX / 10) || (rev == INT_MAX / 10 && d > 7))
+                return 0; 
+            rev=rev*10+d;
+            x/=10;
+        }
+        return (n==static_cast<int>(rev));
+    }
+};
