701c459ef65dae7ef46a632ebb2e39a463a3c41e
Punna Bhargavi <bhargavipunna5714@gmail.com>
2026-08-14 22:39:17 +0530

    Time: 0 ms (100%), Space: 8.8 MB (75.62%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if (n <= 0) return false;
+        // if (n <= 0) return false;
         
-        while (n % 3 == 0) {
+        // while (n % 3 == 0) {
+        //     n /= 3;
+        // }
+        
+        // return n == 1;
+
+        if (n <= 0)
+            return false;
+        while (n % 3 == 0)
             n /= 3;
-        }
-        
-        return n == 1;
+        if (n == 1)
+            return true;
+        return false;
     }
 };
